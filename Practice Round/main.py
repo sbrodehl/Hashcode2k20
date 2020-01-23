b815df1855c3e9fb1a5cc9633a7dc48ced21a51e
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-01-23 11:15:14 +0100

    Add practice problem init and main python files.

---
+++ Practice Round/main.py
@@ -0,0 +1,31 @@
+if __name__ == '__main__':
+    import importlib
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    # need to be
+    parser.add_argument("input", help="input file")
+
+    parser.add_argument("--output", help="output file")
+    parser.add_argument("--solver", type=str, default="example")
+    args = parser.parse_args()
+
+    solver = None
+    # try load the given solver
+    try:
+        solver = importlib.import_module('.'.join(["solver", args.solver]))
+    except ImportError as e:
+        parser.print_help()
+        print(e)
+        exit(1)
+
+    # solver init with filepath
+    solver = solver.Solver(args.input)
+
+    # solve the problem with given input
+    solver.solve()
+
+    # maybe create a solution file
+    if args.output:
+        solver.write(args.output)
