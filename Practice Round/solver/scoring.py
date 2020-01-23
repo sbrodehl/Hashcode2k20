54ddb30532a4f16bd4e7b20ad2c7f479978e0b59
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-01-23 11:16:26 +0100

    Add practice problem basic scoring class.

---
+++ Practice Round/solver/scoring.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import logging
+import numpy as np
+from .parsing import parse_input, parse_output
+
+
+class Score(object):
+    def __init__(self):
+        self.scores = []
+
+    def total(self):
+        return np.array(self.scores).sum()
+
+    def add(self, interest_factor):
+        self.scores.append(interest_factor)
+
+
+def set_log_level(args):
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+def compute_score(file_in, file_out):
+    """
+    Compute score (with bonus) of submission
+    :param file_in: input file
+    :param file_out: output file (solution)
+    :return: Score
+    """
+    # read input and output files
+    input_ = parse_input(file_in)
+    output_ = parse_output(file_out)
+
+    s = Score()
+    return s
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='print score', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file_in', type=str, help='input file e.g. a_example.in')
+    parser.add_argument('file_out', type=str, help='output file e.g. a_example.out')
+    parser.add_argument('--debug', action='store_true', help='set debug level')
+    args = parser.parse_args()
+
+    set_log_level(args)
+
+    score = compute_score(args.file_in, args.file_out)
+
+    print("Score for {}: {} points".format(args.file_out, score.total()))
