474319643ad4318b776c3204ce2fddf4f7d971a4
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-02-20 21:33:11 +0100

    Compute some statistics and print more insights.

--- Online Qualification Round/solver/scoring.py
+++ Online Qualification Round/solver/scoring.py
@@ -43,6 +43,8 @@ def compute_score(file_in, file_out):
     _lib_scan_index = [0] * problem['num_libs']
     _signup_proc_running = -1
     _signup_proc_time_left = 0
+    _signup_proc_last_complete = -1
+    _total_scanned = 0
     current_day = 0
     while current_day < problem['num_days']:
         for libidx, books in solution:
@@ -53,7 +55,7 @@ def compute_score(file_in, file_out):
                 # get the current book ids to be scanned
                 scanning_books = books[_lib_scan_index[libidx]:
                                        _lib_scan_index[libidx] + problem['libs'][libidx]['books_per_day']]
-
+                _total_scanned += len(scanning_books)
                 for b in scanning_books:
                     if not _is_book_scanned[b]:
                         score_ += problem['book_worth'][b]
@@ -71,11 +73,24 @@ def compute_score(file_in, file_out):
             # reset signup process if time evolved
             _lib_is_signed_up[_signup_proc_running] = True
             _signup_proc_running = -1
+            _signup_proc_last_complete = current_day - 1
 
     # output insights
+    logging.info(f"")
     logging.info(f"Submission Insights:")
     _sign_up_stats = 100 * sum(_lib_is_signed_up) / problem['num_libs']
-    logging.info(f"The library signup has been completed for {sum(_lib_is_signed_up)} out of {problem['num_libs']} libraries ({_sign_up_stats}%).")
+    logging.info(f"\tThe library signup has been completed for {sum(_lib_is_signed_up)} out of {problem['num_libs']} libraries ({_sign_up_stats:.2f}%).")
+    logging.info(f"\tThe last library signup process ended on day {_signup_proc_last_complete} of {problem['num_days']} days.")
+    _signup_proc_complete_stats = sum([problem['libs'][idx]['signup_time'] for idx, is_signed_up in enumerate(_lib_is_signed_up) if is_signed_up]) / sum(_lib_is_signed_up)
+    logging.info(f"\tLibrary signup took {_signup_proc_complete_stats:.2f} days on average.")
+    logging.info(f"\tA total of {_total_scanned} books have been scanned.")
+    _scanned_book_worths = [problem['book_worth'][idx] for idx, is_scanned in enumerate(_is_book_scanned) if is_scanned]
+    _scanned_book_avg_worth = sum(_scanned_book_worths) / _total_scanned
+    logging.info(f"\t{sum(_is_book_scanned)} of those books were distinct with an average score of {_scanned_book_avg_worth:.2f}.")
+    _scanned_books_perc = 100 * sum(_is_book_scanned) / problem['num_books']
+    logging.info(f"\tThis is {_scanned_books_perc:.2f}% of the {problem['num_books']} books available across all libraries.")
+    _scanned_book_worth_min, _scanned_book_worth_max = min(_scanned_book_worths), max(_scanned_book_worths)
+    logging.info(f"\tThe minimum score of a scanned book was {_scanned_book_worth_min} and the maximum score of a scanned book was {_scanned_book_worth_max}.")
     return score_
 
 
