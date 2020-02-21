c53872e43d091b470537759aa8e6e7879cc39615
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-02-21 12:34:10 +0100

    Implement score option with logging.

--- Online Qualification Round/main.py
+++ Online Qualification Round/main.py
@@ -33,6 +33,17 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
+    if args.score:
+        scoring = importlib.import_module('.'.join(["solver", "scoring"]))
+        score = scoring.compute_score(args.input, args.output)
+        _nsghs = score.insights
+        logger.info(f"Your submission scored {Markup.BOLD}{score.total():,}{Markup.END} points.")
+        logger.info("")
+        logger.info(f"The library signup has been completed for {Markup.BOLD}{_nsghs['libs_signed_up']:,} out of {_nsghs['num_libs']:,}{Markup.END} libraries ({_nsghs['signup_stats']:.2f}%). The last library signup process ended on day {Markup.BOLD}{_nsghs['signup_proc_finish_day']:,}{Markup.END} of {_nsghs['num_days']:,} days. Library signup took {Markup.BOLD}{_nsghs['signup_proc_complete_stats']:,.2f}{Markup.END} days on average.")
+        logger.info(f"")
+        logger.info(f"A total of {Markup.BOLD}{_nsghs['total_scanned_books']:,}{Markup.END} books have been scanned. {Markup.BOLD}{_nsghs['unique_scanned_books']:,}{Markup.END} of those books were distinct with an average score of {Markup.BOLD}{_nsghs['scanned_book_avg_worth']:,.2f}{Markup.END}. This is {Markup.BOLD}{_nsghs['scanned_books_freq']:.2f}%{Markup.END} of the {_nsghs['num_books']:,} books available across all libraries. The minimum score of a scanned book was {Markup.BOLD}{_nsghs['scanned_book_worth_min']:,}{Markup.END} and the maximum score of a scanned book was {Markup.BOLD}{_nsghs['scanned_book_worth_max']:,}{Markup.END}.")
+        exit(0)
+
     solver = None
     # try load the given solver
     try:
